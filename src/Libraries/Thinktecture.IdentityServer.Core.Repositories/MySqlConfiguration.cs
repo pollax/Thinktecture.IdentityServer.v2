77253c7eba26153dd7b6b43700a3c825975d6c57
Anton Pollak <anton.pollak@netlight.com>
2014-04-11 09:44:21 +0200

    Added support for MySQL database.

---
+++ src/Libraries/Thinktecture.IdentityServer.Core.Repositories/MySqlConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Migrations.History;
+
+namespace Thinktecture.IdentityServer.Core.Repositories
+{
+    public class MySqlHistoryContext : HistoryContext
+    {
+        public MySqlHistoryContext(DbConnection existingConnection, string defaultSchema)
+            : base(existingConnection, defaultSchema)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<HistoryRow>().Property(h => h.MigrationId).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<HistoryRow>().Property(h => h.ContextKey).HasMaxLength(200).IsRequired();
+        }
+    }
+
+    public class MySqlConfiguration : DbConfiguration
+    {
+        public MySqlConfiguration()
+        {
+            SetHistoryContext("MySql.Data.MySqlClient", (conn, schema) => new MySqlHistoryContext(conn, schema));
+        }
+    }
+}
